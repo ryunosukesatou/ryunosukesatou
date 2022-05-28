e109d961f0edc715c0e5ac5e854b30f4fcf0fff9
ryunosukesatou <96858418+ryunosukesatou@users.noreply.github.com>
2022-05-29 06:44:18 +0900

    Add files via upload

--- item.js
+++ item.js
@@ -12,5 +12,18 @@ $(function() {
 		$('nav').fadeOut();
 	});
 
-	
+
+	$('.slider').slick({
+		fade:true,//切り替えをフェードで行う。初期値はfalse。
+		autoplay: true,//自動的に動き出すか。初期値はfalse。
+		autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+		speed:1000,//スライドの動きのスピード。初期値は300。
+		infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+		slidesToShow: 1,//スライドを画面に3枚見せる
+		slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+		arrows: false,//左右の矢印あり
+		dots: false,//下部ドットナビゲーションの表示
+        pauseOnFocus: true,//フォーカスで一時停止を無効
+        pauseOnHover: true,//マウスホバーで一時停止を無効
+	});
 });
